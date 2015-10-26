b1b1189e754ead344753d5e9070660e1c94f2379
Lillian Fleming <fleming.lillian@gmail.com>
2015-10-26 09:52:22 -0500

    Added LillianController

---
+++ centriq-dec-2015-site/Controllers/LillianController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace centriq_dec_2015_site.Controllers
+{
+    public class LillianController : Controller
+    {
+        // GET: Lillian
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
